4961ea5c18fd39240e0df2b355c613de95ab15f9
Jacob Krag <jacob.krag@outlook.dk>
2019-09-25 04:25:52 +0200

    Numbergame - not final result
    Mangler noget JavaScript kode som jeg vil tilføje imorgen

---
+++ js/number.js
@@ -0,0 +1,14 @@
+/*
+  window.onload = function () {
+  var playerNumber;
+  playerNumber = document.querySelector("#number");
+  console.log(playerNumber.value);
+
+  var randomNumber;
+  randomNumber = Math.floor((Math.random() * 1000) + 1); //vælger et random nummer mellem 1 - 1000 og tilføjer det til randomNumber
+  console.log(randomNumber);
+  document.querySelector("#button").addEventListener("click", function() {
+    console.log("Hello");
+  });
+}
+*/
